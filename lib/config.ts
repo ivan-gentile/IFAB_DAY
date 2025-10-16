1d5af745e83843dcdae452c37937d013f46d060b
ivan-gentile <ivogentile@hotmail.it>
2025-10-16 10:17:37 +0200

    Initial commit - project template from another project

---
+++ lib/config.ts
@@ -0,0 +1,40 @@
+import { ColorScheme, StartScreenPrompt, ThemeOption } from "@openai/chatkit";
+
+export const WORKFLOW_ID =
+  process.env.NEXT_PUBLIC_CHATKIT_WORKFLOW_ID?.trim() ?? "";
+
+export const CREATE_SESSION_ENDPOINT = "/api/create-session";
+
+export const STARTER_PROMPTS: StartScreenPrompt[] = [
+  {
+    label: "Come puoi aiutare un socio IFAB?",
+    prompt: "L'utente è un socio IFAB e vuole informazioni sulla fondazione e le attività che la compongono.",
+    icon: "circle-question",
+  },
+  {
+    label: "Come puoi aiutare un dipendente IFAB?",
+    prompt: "L'utente è un DIPENDENTE IFAB",
+    icon: "circle-question",
+  },
+];
+
+export const PLACEHOLDER_INPUT = "Se fossimo nel paleolitico, che innovazione porterebbe IFAB?";
+
+export const GREETING = "Puoi chiedermi quello che vuoi su IFAB";
+
+export const getThemeConfig = (theme: ColorScheme): ThemeOption => ({
+  color: {
+    grayscale: {
+      hue: 220,
+      tint: 6,
+      shade: theme === "dark" ? -1 : -4,
+    },
+    accent: {
+      primary: theme === "dark" ? "#f1f5f9" : "#0f172a",
+      level: 1,
+    },
+  },
+  radius: "round",
+  // Add other theme options here
+  // chatkit.studio/playground to explore config options
+});
